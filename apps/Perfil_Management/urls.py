d0ac1dc717670d029e3336f3ac81b03995e8b97b
Juan Fecci <fecci,juan@gmail.com>
2018-11-08 09:58:41 -0300

    Inicia implememtnacion de Perfil

--- apps/Perfil_Management/urls.py
+++ apps/Perfil_Management/urls.py
@@ -6,7 +6,7 @@ from .views import *
 
 urlpatterns = [
 
-#    url(r'^scans/(?P<pk>\d+)$', PerfilDetail.as_view(), name='Perfil_Detail'),
+    url(r'^perfil/(?P<pk>\d+)$', PerfilDetail.as_view(), name='Perfil_Detail')
 #    url(r'^buscar_error$', Buscar, name='Buscar_Create'),
 #    url(r'^show/$', BuscarList.as_view() , name='show')
 
