7dc3dd76ab4ac6878fbf2426bca4db64400c1a68
Juan Fecci <fecci,juan@gmail.com>
2018-12-03 08:49:19 -0300

    FinalizAvance creacion de viaje

--- apps/Viajes_Management/views.py
+++ apps/Viajes_Management/views.py
@@ -30,12 +30,9 @@ def ViajeCreate(request):
 		#if form.is_valid():
 			viaje = Viaje()
 			viaje.save()
-			viaje.id_viaje = 7
-			viaje.fecha = request.POST.get('fecha_inicio')
-			viaje.hora = request.POST.get('hora_inicio')
+			viaje.estado = -10
 
 			par1 = Parada()
-			par1.id_parada = 10
 			par1.nombre =  request.POST.get('origen')
 			par1.coordenada_x = request.POST.get('lati1')
 			par1.coordenada_y = request.POST.get('long1')
@@ -44,7 +41,6 @@ def ViajeCreate(request):
 			par1.save()
 
 			par2 = Parada()
-			par2.id_parada = 8
 			par2.nombre = request.POST.get('destino')
 			par2.coordenada_x = request.POST.get('lati2')
 			par2.coordenada_y = request.POST.get('long2')
@@ -58,6 +54,9 @@ def ViajeCreate(request):
 			viaje.paradas.add(par1)
 			viaje.paradas.add(par2)
 			usuario.viajes.add(viaje)
+
+			viaje.save()
+			usuario.save()
 			return redirect('http://127.0.0.1:8000/Viajes_Management/viajeCreate/agregar')
 	else:
 		form = ViajeForm()
@@ -90,34 +89,45 @@ def RechazarPlaza(request, viaje_id, trayecto_id):
 
 def AgregarParadas(request):
 	usuario = request.user
-	if request.method == "POST":
-		viaje = usuario.viajes.last()
+	viaje = usuario.viajes.last()
+
+	if request.method == "POST" and viaje.estado != -10:
+		
 		par2 = Parada()
-		par2.id_parada = Parada.objects.count() +1
-		par2.nombre = request.POST.get('parada')
-		#par2.coordenada_x = request.POST.get('lati1')
-		#par2.coordenada_y = request.POST.get('long1')
-		par2.coordenada_x = 200
-		par2.coordenada_y = 200
+		par2.nombre = request.POST.get('origen')
+		par2.coordenada_x = float(request.POST.get('lati1'))
+		par2.coordenada_y = float(request.POST.get('long1'))
+		#par2.coordenada_x = 200
+		#par2.coordenada_y = 200
 		par2.fecha = request.POST.get('fecha_inicio')
 		par2.hora = request.POST.get('hora_inicio')
 		par2.save()
 		viaje.paradas.add(par2)
 
-		return render(request,'Viajes_Management/agregar_paradas.html')
+		viaje.save()
+
+		return render(request,'Viajes_Management/agregar_parada.html', {})
 	else:
-		form = ViajeForm()
-		return render(request, 'Viajes_Management/agregar_paradas.html', {'form': form} )		
+		viaje.estado = -9
+		viaje.save()
+		return render(request, 'Viajes_Management/agregar_parada.html', {} )		
 
 def Viaje2(request):
 	usuario = request.user
-	if request.method == "POST":
+	viaje = usuario.viajes.last()
+
+	if request.method == "POST" and viaje.estado != -9:
 		form = ViajeForm(request.POST)
-		viaje = usuario.viajes.last()
-		viaje.tarifaPreferencia = form['tarifa']
-		viaje.maletero = form['maletero']
-		viaje.mascota = form['mascota']
-		return redirect('/')
+		if form.is_valid():
+
+			viaje.tarifaPreferencias = form.cleaned_data['tarifa']
+			viaje.maletero = form.cleaned_data['maletero']
+			viaje.mascota = form.cleaned_data['mascota']
+
+			viaje.save()
+			return render(request, 'Viajes_Management/correcto2.html')
 	else:
 		form = ViajeForm()
+		viaje.estado = -8
+		viaje.save()
 		return render(request, 'Viajes_Management/Viaje2.html', {'form': form} )		
